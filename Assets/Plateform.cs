201229da3b7c37607e56e8f236511fa6e1f723b1
DESKTOP-8LDL4R4\lmart <lohmt0962@gmail.com>
2020-11-26 14:13:49 +0100

    project plateform

---
+++ Assets/Plateform.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plateform : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    Vector3 direction = new Vector3(0f, 0f, 1f);
+    void FixedUpdate()
+    {
+        Vector3 currentPosition = transform.position;
+        float speed = 2f;
+
+        if (currentPosition.z > 7)
+        {
+            direction = new Vector3(0f, 0f, -1f);
+        }
+        if (currentPosition.z < -1)
+        {
+            direction = new Vector3(0f, 0f, 1f);
+        }
+
+        Vector3 newPosition = currentPosition + direction * speed * Time.deltaTime;
+
+        rigidbody.MovePosition(newPosition);
+    }
+}
---
+++ Assets/Respawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Respawn : MonoBehaviour
+{
+    Vector3 Position;
+
+    void Start()
+    {
+        Position = transform.position;
+    }
+
+    private void Update()
+    {
+        if (transform.position.y<-2)
+        {
+            transform.position = Position;
+        }
+    }
+}
