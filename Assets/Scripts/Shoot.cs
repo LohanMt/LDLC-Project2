04c249ce90cc99d913ec35aef582ae78b6bca4fd
DESKTOP-8LDL4R4\lmart <lohmt0962@gmail.com>
2020-11-30 13:14:43 +0100

    project plateform 2

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour
+{
+    public Rigidbody projectile;
+    public Transform Spawnpoint;
+
+    public GameObject Sphere;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Rigidbody clone;
+            clone = (Rigidbody)Instantiate(projectile, Spawnpoint.position, projectile.rotation);
+
+            clone.velocity = Spawnpoint.TransformDirection(Vector3.forward * 20);
+        }
+    }
+}
